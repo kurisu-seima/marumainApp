2b1bc0d5e1398b430323988e92932bd7b7811961
kurisu-seima <binbingin0929@gmail.com>
2020-12-22 22:34:56 +0900

    Initial Commit

---
+++ MarumainApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarumainApp
+//
+//  Created by 栗須星舞 on 2020/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
